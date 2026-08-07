02c417e50e8ccb20e5685dd6e81e217ad4a88964
STUDIO1923 <home.studio1923@gmail.com>
2026-08-07 23:31:01 +0700

    Replace broken mobile friend panel

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE="samati-pwa-v19";
+const CACHE="samati-pwa-v20";
 const SHELL=["/","/manifest.webmanifest","/samati-icon-192.png","/samati-icon-512.png"];
 
 self.addEventListener("install",event=>{
